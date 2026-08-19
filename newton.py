0051309456158cff477f5594ea3c364e9d907f0c
zheshanliu-lab <zheshan_liu@berkeley.edu>
2026-08-19 14:06:48 -0700

    Update Newton optimization with doc strings and max_iter

--- newton.py
+++ newton.py
@@ -1,12 +1,26 @@
 def derivative(f, x, h=1e-5):
+    """Approximate the first derivative of f at x using forward difference."""
     return (f(x + h) - f(x)) / h
 
 def second_derivative(f, x, h=1e-5):
+    """Approximate the second derivative of f at x."""
     return (derivative(f, x + h, h) - derivative(f, x, h)) / h
 
-def newton_optimizatio(f, x0):
+def optimize(f, x0, h=1e-5, max_iter = 1000):
+    """ Find a local minimum of f using Newton's method starting at x0.
+        Use methods above to compute the first derivative and second derivative.
+
+        Parameters:
+        f -- function to be optimized
+        x0 -- initial value to start with
+        h -- value used to compute derivatives, default set to 1e-5
+        max_iter -- maximum number of iterations to avoid inifinite loop
+
+        Returns:
+        Estimated location of a local minimum.
+    """
     x = x0
-    while True:
+    for _ in range(max_iter):
         first_derivative = derivative(f, x)
         second_derivative_value = second_derivative(f, x)
 
@@ -16,3 +30,5 @@ def newton_optimizatio(f, x0):
             return x_new
 
         x = x_new
+
+        return x
